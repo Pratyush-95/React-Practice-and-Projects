a519af9317e1724a45d843692524e55e167d1f07
Pratyush Kumar <134200485+Pratyush-95@users.noreply.github.com>
2026-01-06 22:27:05 +0530

    Initial commit - Generate Random Number app

---
+++ generate-random-no/src/App.jsx
@@ -0,0 +1,16 @@
+
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import NumberGenerator from './Component/NumberGenerator'
+
+function App() {
+   
+  return (
+    <div>
+      <NumberGenerator />
+    </div>
+  )
+}
+
+export default App
